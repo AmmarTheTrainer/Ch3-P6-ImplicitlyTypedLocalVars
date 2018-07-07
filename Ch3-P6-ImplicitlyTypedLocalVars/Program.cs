01ecea0c5cc06bc33c10b3996bbfca86f30a28b3
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-07 07:00:08 -0700

    this is a coomit from talha < page 2 >

--- Ch3-P6-ImplicitlyTypedLocalVars/Program.cs
+++ Ch3-P6-ImplicitlyTypedLocalVars/Program.cs
@@ -14,7 +14,7 @@ namespace Ch3_P6_ImplicitlyTypedLocalVars
             //DeclareImplicitVars();
             //RestrictionsOnVarKeyword();
             //ImplicitTypingIsStrongTyping();
-            LinqQueryOverInts();
+            //LinqQueryOverInts();
 
             Console.ReadLine();
         }
