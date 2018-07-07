447d5038385b2c8abff7c35db05d77fbf2b835e8
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-07 04:20:20 -0700

    final testing commit  7

--- Ch3-P6-ImplicitlyTypedLocalVars/Program.cs
+++ Ch3-P6-ImplicitlyTypedLocalVars/Program.cs
@@ -12,14 +12,6 @@ namespace Ch3_P6_ImplicitlyTypedLocalVars
         {
             Console.WriteLine(" welcome back to full stack .NET batch 1 ");
 
-            Console.WriteLine(" Hello friends chaye pe lo...! ");
-
-            Console.WriteLine(" hello friends 1 bar phr chae pe lo... < commit no 4 >  ");
-
-            Console.WriteLine(" this is for commit no 5 ");
-
-            Console.WriteLine(" this is for commit 6 < 1 line addd >  ");
-
             Console.ReadLine();
         }
     }
