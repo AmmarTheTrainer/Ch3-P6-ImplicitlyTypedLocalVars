43776f74a77d1491626ed4dfb9e154b0c7835ec3
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-07 03:55:33 -0700

    first commit by att
    testing in progress

---
+++ Ch3-P6-ImplicitlyTypedLocalVars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch3_P6_ImplicitlyTypedLocalVars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" welcome back to full stack .NET batch 1 ");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
